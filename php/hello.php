7d46a9bf4152b7022ae4b31137fe79e50eba7b70
Zyris Shakir <zeyeland@gmail.com>
2019-04-09 10:16:17 -0500

    if_else and _GET superglobal practice

--- php/hello.php
+++ php/hello.php
@@ -1,11 +1,13 @@
 <?php
-
+//$msg2 = 'Hello $name';
+//$msg3 = 'Hello '.$name;
 //echo "Hello World";
 $name = 'Bob Smith';
-$msg = "Hello {$name}";
-$msg2 = 'Hello $name';
-$msg3 = 'Hello '.$name;
-
+$greeting = "Hello {$name}";
+$count = 5;
+$price = 3.00;
+$total=($count*$price);
+$msg = "I see you have {$count} oranges, that will be \${$total}";
 ?>
 
 <!DOCTYPE html>
@@ -16,9 +18,7 @@ $msg3 = 'Hello '.$name;
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <h1> <?php echo 'Hello World'; ?> </h1>
-        <h2> <?php echo $msg3; ?> </h2>
-        <h3> <?php echo $msg3; ?> </h3>
-        <h4> <?php echo $msg3; ?> </h4>
+        <h1> <?php echo $greeting; ?> </h1>
+        <h2> <?php echo $msg; ?> </h2>
     </body>
 </html>
